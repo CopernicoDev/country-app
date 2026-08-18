a2365400518ea86984ac38d4e1bb664190f95242
CopernicoDev <giraldomunoznicolas900@gmail.com>
2026-08-18 20:38:49 +0100

    Refactorizando el componente by-capital para que use rxResource

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { Search } from "../../components/search/search";
 import { CountryList } from "../../components/country-list/country-list";
 import { CountryService } from '../../services/country.service';
 import { RESTCountry } from '../../interfaces/rest-countries';
 import { CountryInterface } from '../../interfaces/country.interface';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -13,29 +15,13 @@ import { CountryInterface } from '../../interfaces/country.interface';
 })
 export class ByCapitalPage {
   countryServices = inject(CountryService)
+  query = signal<string>('')
 
-  isLoading = signal(false);
-  isError = signal<String | null>(null)
-  countries = signal<CountryInterface[]>([])
-
-
-  getCapital(query: string) {
-    if (this.isLoading()) return
-
-    this.isLoading.set(true)
-    this.isError.set(null)
-
-    this.countryServices.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false)
-        this.countries.set(countries)
-      },
-      error: (message) => {
-        this.isLoading.set(false)
-        this.countries.set([])
-        this.isError.set(`Country not found : ${query}`)
-
-      }
-    })
-  }
+  countryResource = rxResource<CountryInterface[], { query: string }>({
+    params: () => ({ query: this.query() }),
+    stream: ({ params }) => {
+      if (!params.query) return of([]);
+      return this.countryServices.searchByCapital(params.query);
+    }
+  })
 }
